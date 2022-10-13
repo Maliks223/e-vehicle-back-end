bd31a38030e6e0d6ba248b37ce5388a90583686e
malik <malikakkawi223@gmail.com>
2022-10-13 09:57:07 +0300

    edit server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const colors = require('colors');
 const mongoose = require('mongoose');
 require('dotenv').config()
 const express = require("express");
-const port = process.env.PORT || 5000
+const port = process.env.PORT
 const connectDB = require('./DataBase/db')
 const user = require('./Routes/userRoute')
 const post = require('./Routes/postRoute')
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 // app.use(upload.array());
-app.use("/public", express.static("uploads"));
+app.use("/public", express.static("./uploads"));
 
 app.use("/user", user);
 app.use("/post", post);
